d48a4fc4d9060b3faf88dfe8858cfd5ff5ddcd2d
kilianrdgs <kilian.rdgs@free.fr>
2024-11-14 17:04:50 +0100

    enigma crypt

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,14 @@
 package org.example;
 
-import org.example.menus.homeMenu;
-
 import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
 
 public class Main {
     public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
-     //homeMenu.getHomeMenu();
-       Hash.hash("hello", "SHA-256");
+
+       // Enigma.crypt("hello", "hel");
+       // Enigma.decrypt("bxoou", "leh");
+        // homeMenu.getHomeMenu();
+        // Hash.hash("hello", "SHA-256");
     }
 }
